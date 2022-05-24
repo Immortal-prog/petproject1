e4633cc87a271c89ba9a2e4c275f583db54b8a5a
Vladyslav Pokoiuk <vladpokoyuk@gmail.com>
2022-05-24 23:27:25 +0300

    commited

---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  const [hello, sethello] = useState(second);
+  return (
+    <div>
+      <h1 className="drag">Hello world</h1>
+    </div>
+  );
+}
+export default Header;
